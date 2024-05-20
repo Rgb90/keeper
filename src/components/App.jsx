dda388cb87160429664c73b86e860b4c007bb1d9
Rgb90 <143276231+Rgb90@users.noreply.github.com>
2024-05-20 17:16:32 +0300

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
-import './App.css';
 import Header from './Header'
 import Footer from './Footer'
+import Note from './Note';
 
 
 function App() {
-  
+
   return (
     <>
       <Header />
+      <Note />
       <Footer />
     </>
   )
