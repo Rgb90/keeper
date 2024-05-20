79e74951650e5ed66bde442077751b7747f37484
Rgb90 <143276231+Rgb90@users.noreply.github.com>
2024-05-20 15:46:41 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Footer() {
+    const date = new Date().getFullYear()
+
+    return (
+        <footer>
+            <p>Copright © {date}</p>
+        </footer>
+    )
+}
+
+export default Footer;
